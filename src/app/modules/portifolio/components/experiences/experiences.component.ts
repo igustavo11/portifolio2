efb07e8a86878910ce19bda1162d34a921041116
igustavo11 <131629449+igustavo11@users.noreply.github.com>
2025-03-15 13:32:19 -0300

    feat: update experiences

--- src/app/modules/portifolio/components/experiences/experiences.component.ts
+++ src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -6,32 +6,30 @@ import { IExperiences } from '../interface/IExperiences.interfaces';
   standalone: true,
   imports: [],
   templateUrl: './experiences.component.html',
-  styleUrl: './experiences.component.scss'
+  styleUrl: './experiences.component.scss',
 })
 export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
-      summary:{
+      summary: {
         strong: 'Suporte Técnico',
-        p: 'Inovar Sistemas | Mai 2024 - Present'
+        p: 'Inovar Sistemas | Mai 2024 - Present',
       },
-      text: 'Desde maio de 2024, atuo como suporte técnico na Inovar Sistemas, aplicando suporte do software Advanced, para os clientes'
+      text: 'Sou responsável pelo suporte de TI, ajudando os usuários a resolver problemas relacionados ao sistema e prestando assistência técnica. Minha rotina envolve orientar as pessoas no uso correto do sistema, solucionar dúvidas e corrigir erros em consultas SQL para garantir que os dados sejam recuperados de forma precisa e eficiente. Com isso, desenvolvi habilidades de comunicação, resolução de problemas, conhecimento em SQL e entendimento das necessidades do usuário, competências que considero valiosas para uma transição para a área de desenvolvimento, onde posso criar soluções que realmente atendam às expectativas dos usuários.',
     },
     {
-      summary:{
+      summary: {
         strong: 'Especialista em suporte de TI',
-        p: 'Anhanguera | Mar 2024 - Mai 2024'
+        p: 'Anhanguera | Mar 2024 - Mai 2024',
       },
-      text: 'Durante meu período na Anhanguera de março de 2024 a maio de 2024, atuei como especialista em suporte de TI, aplicando suporte em infraestrutura computacional, como servidores e redes'
+      text: 'Fui responsável pela infraestrutura na faculdade, atuando com manutenção de hardware e configurações de rede no domínio. Minha função envolvia garantir a estabilidade da rede, realizar manutenções preventivas e corretivas, e assegurar que os sistemas da instituição funcionassem sem falhas. Com isso, adquiri experiência prática em infraestrutura de TI, resolução de problemas e gestão de redes, habilidades que complementam meu interesse em desenvolvimento, especialmente no que diz respeito à integração de sistemas e otimização de ambientes tecnológicos.',
     },
     {
-      summary:{
+      summary: {
         strong: 'Técnico de Telecomunicações',
-        p: 'RadioTelecom | Jan 2020 - Mar 2024'
+        p: 'RadioTelecom | Jan 2020 - Mar 2024',
       },
-      text: 'Durante meu período na RadioTelecom de janeiro de 2020 a março de 2024, atuei como técnico de telecomunicações dando manutenção e programando rádios de comunicação P25'
+      text: 'Durante meu período na RadioTelecom de janeiro de 2020 a março de 2024, atuei como técnico de telecomunicações dando manutenção e programando rádios de comunicação P25',
     },
-
-  ])
-
+  ]);
 }
